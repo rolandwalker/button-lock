891fcc393ecb5218547dfb85366c10c8f06d6b57
Roland Walker <walker@pobox.com>
2012-09-21 13:35:39 -0400

    include build and autoload with test target include stub test files no individual tests defined, though

---
+++ ert-tests/wiki-nav-test.el
@@ -0,0 +1,21 @@
+
+;; requires and setup
+
+(require 'button-lock)
+(require 'wiki-nav)
+
+;;; this is a stub - no tests defined
+
+;;
+;; Emacs
+;;
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; coding: utf-8
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+;;
+
+;;; wiki-nav-test.el ends here
