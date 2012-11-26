cf03bc69ea56af87c7b7c4c581b2ffb431be88aa
Roland Walker <walker@pobox.com>
2012-11-26 12:27:04 -0500

    improve compat of called-interactively-p macro including trunk (to be 24.3) and versions with only `interactive-p`

--- button-lock.el
+++ button-lock.el
@@ -399,9 +399,15 @@ This variable should be set by calling
 
 Optional KIND is as documented at `called-interactively-p'
 in GNU Emacs 24.1 or higher."
-  (if (eq 0 (cdr (subr-arity (symbol-function 'called-interactively-p))))
-      '(called-interactively-p)
-    `(called-interactively-p ,kind)))
+  (cond
+    ((not (fboundp 'called-interactively-p))
+     '(interactive-p))
+    ((condition-case nil
+         (progn (called-interactively-p 'any) t)
+       (error nil))
+     `(called-interactively-p ,kind))
+    (t
+     '(called-interactively-p))))
 
 ;;; compatibility functions
 
