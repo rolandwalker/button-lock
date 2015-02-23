9a8f950e06ff222b667c38facf4fb9d6ef6f1d8e
Roland Walker <walker@pobox.com>
2015-02-23 08:19:23 -0500

    remove second copyright date

--- wiki-nav.el
+++ wiki-nav.el
@@ -275,9 +275,6 @@
 ;;
 ;; Simplified BSD License
 ;;
-;; Copyright (c) 2011-15, Roland Walker
-;; All rights reserved.
-;;
 ;; Redistribution and use in source and binary forms, with or
 ;; without modification, are permitted provided that the following
 ;; conditions are met:
